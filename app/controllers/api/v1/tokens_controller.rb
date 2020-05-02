5bdc68139c24e0d04cf2d9dc448a55c1d3dbe750
Suyesh Bhandari <suyeshb@gmail.com>
2020-05-01 20:39:27 -0400

    Json web token

---
+++ app/controllers/api/v1/tokens_controller.rb
@@ -0,0 +1,4 @@
+class Api::V1::TokensController < ApplicationController
+  def create
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      get 'tokens/create'
+    end
+  end
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :users, only: %i[show create update destroy]
+      resources :tokens, only: %i[create]
     end
   end
 end
