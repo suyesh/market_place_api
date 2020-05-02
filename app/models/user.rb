af42fed8618b5031be28aff53da228a622da161d
Suyesh Bhandari <suyeshb@gmail.com>
2020-05-01 20:02:08 -0400

    Added user resources;

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+class User < ApplicationRecord
+  validates :email, uniqueness: true
+  validates_format_of :email, with: /@/
+  validates :password_digest, presence: true
+  has_secure_password
+end
