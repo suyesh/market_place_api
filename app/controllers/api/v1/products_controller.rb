6901b3b448b77dafeb9873e6db5e489e0460ce02
Suyesh Bhandari <suyeshb@gmail.com>
2020-05-05 21:53:49 -0400

    Finishes modeling the product model along with user associations

---
+++ app/controllers/api/v1/products_controller.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+class Api::V1::ProductsController < ApplicationController
+  def index
+    render json: Product.all
+  end
+
+  def show
+    render json: Product.find(params[:id])
+  end
+end
